be5833f90c09b6c46683b5c442d1c0846634125d
Saurabh Lakhanpal <firsty111@gmail.com>
2025-10-13 01:19:57 +0530

    Linter fix from flask

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -12,12 +12,13 @@ sentiment_analyzer_url = os.getenv(
     'sentiment_analyzer_url',
     default="http://localhost:5050/")
 
+
 def get_request(endpoint, **kwargs):
     # Add code for get requests to back end
     params = ""
-    if(kwargs):
-        for key,value in kwargs.items():
-            params=params+key+"="+value+"&"
+    if kwargs:
+        for key, value in kwargs.items():
+            params = params+key+"="+value+"&"
 
     request_url = backend_url+endpoint+"?"+params
 
@@ -26,14 +27,13 @@ def get_request(endpoint, **kwargs):
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
         return response.json()
-    except:
+    except Exception:
         # If any error occurs
         print("Network exception occurred")
 
-# def analyze_review_sentiments(text):
-# request_url = sentiment_analyzer_url+"analyze/"+text
-# Add code for retrieving sentiments
+
 def analyze_review_sentiments(text):
+    # Add code for retrieving sentiments
     request_url = sentiment_analyzer_url+"analyze/"+text
     try:
         # Call get method of requests library with URL and parameters
@@ -43,13 +43,13 @@ def analyze_review_sentiments(text):
         print(f"Unexpected {err=}, {type(err)=}")
         print("Network exception occurred")
 
-# def post_review(data_dict):
-# Add code for posting review
+
 def post_review(data_dict):
+    # Add code for posting review
     request_url = backend_url+"/insert_review"
     try:
-        response = requests.post(request_url,json=data_dict)
+        response = requests.post(request_url, json=data_dict)
         print(response.json())
         return response.json()
-    except:
+    except Exception:
         print("Network exception occurred")
